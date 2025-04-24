311d15dc819c8f14dd06514fe596372fe33d9725
unknown <g00472990>
2025-04-24 19:52:13 +0100

    work

--- analysis.py
+++ analysis.py
@@ -12,7 +12,7 @@ data = pd.read_csv('iris.csv')
 
 # Open a file to write the summary
 with open("summary.txt", "w") as summary_file:
-    
+
 # Go through each numeric column in the dataset
 # Select_dtypes() method in Pandas to select columns based on their data type 
 # ref:https://www.w3schools.com/python/pandas/ref_df_select_dtypes.asp
@@ -57,3 +57,39 @@ with open("summary.txt", "w") as summary_file:
 
 # Print summary to summary.txt to display all information
 print("Summary to summary.txt")
+
+# 2. Save a histogram of each variable to png files
+
+# Import numpy and matplotlib for histogram generation
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+# Load the Dataset
+iris = load_iris()
+data = iris.data 
+feature_names = iris.feature_names
+
+
+# Create a histogram for each feature and save to file.png
+# Create and save a histogram for each feature
+for i in range(len(feature_names)):
+
+# Set figure size for a more clear Histogram
+# Set bins to 10 for and colours to green with edge colour black for a clean Histogram
+# Set title for 'Histogram of feature name' 
+# Set x axis to include feature name
+# Set y axis to show Frequency 
+    plt.figure(figsize=(8, 5))
+    plt.hist(data[:, i], bins=10, color='green', edgecolor='black')
+    plt.title(f'Histogram of {feature_names[i]}')
+    plt.xlabel(feature_names[i])
+    plt.ylabel('Frequency')
+
+# Use feature_names to get name of current feature
+# Use replace to change all spaces into underscores, remove any opening and closing brackets
+    safe_name = feature_names[i].replace(" ", "_").replace("(", "").replace(")", "")
+# Use F-string to insert variable into saved file eg:sepal length    
+    plt.savefig(f'{safe_name}_file.png')
+    
+# 3. Output a scatter plot of each pair of variables. 
